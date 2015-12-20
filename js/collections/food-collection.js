b284fbfe7e202cc6f2e491014c5533c4221b91db
James T Peterson <jpeterson903@yahoo.com>
2015-12-20 14:11:40 -0800

    feat: test ajax request results

--- js/collections/food-collection.js
+++ js/collections/food-collection.js
@@ -1,1 +1,24 @@
 var app = app || {};
+
+app.FoodCollection = Backbone.Collection.extend({
+	model: app.FoodItem,
+	initialize: function(models, options) {
+		this.query = options.query;
+	},
+
+	url: function() {
+		var appId = "3b3f2d32";
+		var appKey = "53a3a37d31928d1cb223a9867554e6cd";
+
+		return "https://api.nutritionix.com/v1_1/search/" + this.query + "?fields=item_name%2Cnf_calories" + "&appId=" + appId + "&appKey=" + appKey;
+	},
+
+	parse: function(data) {
+		console.log(data.hits[0]);
+		return data.hits;
+	}
+});
+
+var cheeseBurger = new app.FoodCollection([], {query: 'cheese burger'});
+cheeseBurger.fetch();
+
