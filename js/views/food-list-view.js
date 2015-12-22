693f068b9f0c05f4d300c827ed4ac8a177d2f938
James T Peterson <jpeterson903@yahoo.com>
2015-12-22 13:24:41 -0800

    feat: test rendering a collection with underscore template

--- js/views/food-list-view.js
+++ js/views/food-list-view.js
@@ -1,1 +1,28 @@
 var app = app || {};
+
+app.FoodListView = Backbone.View.extend({
+
+	initialize: function() {
+		_.bindAll(this, 'render');
+
+		this.collection = new FoodCollection({name: 'starbucks coffee'});
+
+		var self = this;
+		this.collection.fetch({
+			success: function() {
+				self.render();
+			}
+		});
+	},
+
+	template: _.template($('#foodTemplate').html()),
+
+	render: function() {
+
+		$(this.el).html(this.template({ food: this.collection.toJSON() }));
+	}
+});
+
+var foodApp = new app.FoodListView ({
+	el: $('body')
+});
