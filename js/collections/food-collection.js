f0430533f1a3888398788fd6d2f426b179bb9b40
James T Peterson <jpeterson903@yahoo.com>
2015-12-22 13:22:52 -0800

    fix: change query to name

--- js/collections/food-collection.js
+++ js/collections/food-collection.js
@@ -3,24 +3,26 @@ var app = app || {};
 var FoodCollection = Backbone.Collection.extend({
 	model: app.FoodItem,
 
-	initialize: function(models, options) {
-		console.log('makin collection of food')
-		this.query = options.query;
+	initialize: function(options) {
+		console.log('makin collection of food');
+		this.name = options.name;
 	},
 
 	url: function() {
 		var appId = "3b3f2d32";
 		var appKey = "53a3a37d31928d1cb223a9867554e6cd";
 
-		return "https://api.nutritionix.com/v1_1/search/" + this.query + "?fields=item_name%2Cnf_calories" + "&appId=" + appId + "&appKey=" + appKey;
+		return "https://api.nutritionix.com/v1_1/search/" + this.name + "?fields=item_name%2Cnf_calories" + "&appId=" + appId + "&appKey=" + appKey;
 	},
 
 	parse: function(response) {
-		console.log(response.hits[0]);
+		//console.log(response.hits[0]);
 		return response.hits;
 	}
 });
 
-var cheeseBurger = new FoodCollection([], {query: 'cheese burger'});
-cheeseBurger.fetch();
+//var Food = new FoodCollection({name: ''});
+//var bread = new FoodCollection({query: 'bread'});
+//Food.fetch()
+//bread.fetch();
 
