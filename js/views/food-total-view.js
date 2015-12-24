c8adb34dfb74a4374a0bad153fd6fe0308a59cef
James T Peterson <jpeterson903@yahoo.com>
2015-12-24 13:22:15 -0800

    feat: each model now has it's own view and is rendered

--- js/views/food-total-view.js
+++ js/views/food-total-view.js
@@ -1,1 +1,33 @@
 var app = app || {};
+
+app.FoodTotalView = Backbone.View.extend({
+
+	//el: '#foodContainer',
+	tagName: 'li',
+
+	// template: '<ul id="food-list"></ul>',
+
+
+	events: {
+		'click' : 'getModel'
+	},
+
+	initialize: function(options) {
+		console.log('food total view initialized');
+		this.model = options.model;
+
+	},
+
+	getModel: function() {
+		console.log(this.model);
+	},
+
+	render: function() {
+
+		this.$el.html(this.model.attributes.fields.item_name + " | calories: " + this.model.attributes.fields.nf_calories);
+		return this;
+	}
+
+});
+
+//var foodTotal = new app.FoodTotalView();
