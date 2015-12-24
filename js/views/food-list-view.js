d7f8b2451accdf18913e78225b9e494b32a67a81
James T Peterson <jpeterson903@yahoo.com>
2015-12-23 17:02:09 -0800

    feat: add features to search health api on enter key press and render response.
    Add #searchContainer as the DOM element.
    Pressing the enter key inside the input tag fetches a collection.
    On successful fetch the collection is rendered.
    Renders the collection inside an empty div tag with underscore running for
    each on the collection.

--- js/views/food-list-view.js
+++ js/views/food-list-view.js
@@ -2,27 +2,71 @@ var app = app || {};
 
 app.FoodListView = Backbone.View.extend({
 
+	el: '#searchContainer',
+
+	events: {
+		'keypress #searchFood' : 'getFood'
+	},
+
 	initialize: function() {
+
+		//this.collection.on("add", this.render, this);
+
+		console.log('running initialize');
+		// this.listenTo(this.collection, 'change', this.render);
+		// this.listenTo(this.collection, 'destroy', this.remove);
+
+		// var title = this.$el.find('input').val();
+
+		// _.bindAll(this, 'render');
+
+		// this.collection = new FoodCollection({name: 'grill'});
+
+		// var self = this;
+		// this.collection.fetch({
+		// 	success: function() {
+		// 		self.render();
+		// 	}
+		// });
+	},
+
+	getFood: function(e) {
+
+
 		_.bindAll(this, 'render');
+		if (e.which === ENTER_KEY) {
+			console.log('getting food');
+			var food_name = $('#searchFood').val();
 
-		this.collection = new FoodCollection({name: 'starbucks coffee'});
+			this.collection = new FoodCollection({name: food_name});
 
-		var self = this;
-		this.collection.fetch({
-			success: function() {
-				self.render();
-			}
-		});
+			var self = this;
+			this.collection.fetch({
+				success: function() {
+					self.render();
+				}
+			});
+		}
+
+
 	},
 
 	template: _.template($('#foodTemplate').html()),
 
 	render: function() {
 
-		$(this.el).html(this.template({ food: this.collection.toJSON() }));
+		// $('#itemContainer').html('');
+
+		//console.log($(this.el).html());
+
+		$('#foodContainer').html(this.template({ food: this.collection.toJSON() }));
+		//$('#foodContainer'.el).html(this.template({ food: this.collection.toJSON() }));
 	}
 });
 
-var foodApp = new app.FoodListView ({
-	el: $('body')
-});
+// var foodApp = new app.FoodListView ({
+// 	el: $('#searchContainer')
+// });
+
+var ENTER_KEY = 13;
+var foodApp = new app.FoodListView();
