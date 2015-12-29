d0ceae782c17d92b0f2c74ec97df61d318c0143f
James T Peterson <jpeterson903@yahoo.com>
2015-12-28 19:57:05 -0800

    fix: comment out pseudo code

--- js/models/food-model.js
+++ js/models/food-model.js
@@ -1,11 +1,11 @@
 var app = app || {};
 
 app.FoodItem = Backbone.Model.extend({
-	initialize: function(options) {
-		console.log('makin food');
-	}
-});
 
-var milk = new app.FoodItem({ name: 'milk', calories: 100 });
-var cheese = new app.FoodItem({ name: 'cheese', calories: 200 });
-var bread = new app.FoodItem({ name: 'bread', calories: 150 });
+	initialize: function() {
+		console.log('makin food');
+	},
+});
+// var milk = new app.FoodItem({ name: 'milk', calories: 100 });
+// var cheese = new app.FoodItem({ name: 'cheese', calories: 200 });
+// var bread = new app.FoodItem({ name: 'bread', calories: 150 });
