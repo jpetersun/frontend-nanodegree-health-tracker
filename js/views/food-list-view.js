63932dc85335c2c7cfb73f53c2cd6f09b4922435
James T Peterson <jpeterson903@yahoo.com>
2015-12-29 17:38:18 -0800

    style: remove commented out code

--- js/views/food-list-view.js
+++ js/views/food-list-view.js
@@ -2,14 +2,12 @@ var app = app || {};
 
 app.FoodListView = Backbone.View.extend({
 
-	//el: '#foodContainer',
 	tagName: 'li',
 
 	className: 'search-results',
 
 	events: {
 		'click' : 'getModel',
-		// 'click .selected-items'
 	},
 
 	initialize: function(options) {
@@ -18,19 +16,10 @@ app.FoodListView = Backbone.View.extend({
 		this.listenTo(this.model, 'destroy', this.remove);
 	},
 
-	// newItem: function() {
-	// 	return {
-	// 		title: this.model.attributes.fields.item_name,
-	// 		calories: this.model.attributes.fields.nf_calories
-	// 	}
-	// },
-
 	getModel: function() {
-		//var totalView;
+
 		var selectedItem = this.model;
-		//totalView = new app.FoodTotalView({model: this.model});
-		// this.$el.find('#total-list').append(totalView.render().el);
-		//$('#total-list').append(totalView.render().el);
+
 		app.selectedFoods.add({
 			title: this.model.attributes.fields.item_name,
 			calories: this.model.attributes.fields.nf_calories
@@ -43,7 +32,4 @@ app.FoodListView = Backbone.View.extend({
 		this.$el.html("<strong>" + this.model.attributes.fields.item_name + "</strong>" + " | calories: " + this.model.attributes.fields.nf_calories);
 		return this;
 	}
-
 });
-
-//var foodTotal = new app.FoodTotalView();
